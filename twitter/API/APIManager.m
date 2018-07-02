a85fd801034d4aaee5a354507fdd8c96338b8b61
Tiffany Ma <tma@fb.com>
2018-07-02 14:08:19 -0700

    added API keys and login

--- twitter/API/APIManager.m
+++ twitter/API/APIManager.m
@@ -9,8 +9,8 @@
 #import "APIManager.h"
 
 static NSString * const baseURLString = @"https://api.twitter.com";
-static NSString * const consumerKey = // Enter your consumer key here
-static NSString * const consumerSecret = // Enter your consumer secret here
+static NSString * const consumerKey = @"j3zPggX3eTzaFbJfIg1AFx1Jr";
+static NSString * const consumerSecret = @"mFg6eXkuyjIivpE2KST5G4KCGYWNGx4XfXYzicZ4mu1pARNxYP";
 
 @interface APIManager()
 
